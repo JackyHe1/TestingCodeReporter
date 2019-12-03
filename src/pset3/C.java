56eca871c0802fe04a6a52041321abad94cb7a67
Dong He <jackyhekeep@gmail.com>
2019-12-02 20:54:44 -0600

    add logic to handle for loop paths generation

--- src/pset3/C.java
+++ src/pset3/C.java
@@ -4,11 +4,12 @@ public class C {
     int max(int x, int y) {
         int a = 1;
         int b = 2;
-
-        if(x > 2) {
+//        if(x > 2) {
+//            a += b;
+//        }
+        for(int i = 0; i < x; i++) {
             a += b;
         }
-
         if (x < y) {
             return y;
         } else {
